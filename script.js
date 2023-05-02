aaa8eb7f1294fb09fb67df4879e7b977d74504e4
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-05-02 14:14:35 -0300

    Em desenvolvimento
    Inserido informações nos primeiros botões

---
+++ script.js
@@ -0,0 +1,24 @@
+function botaoCalculadora(){
+    window.location.href = 'https://andreaasilva.github.io/calculadora/'
+}
+
+var primeiro_botao = document.getElementById('projetos__botao__primeiro__projeto')
+primeiro_botao.addEventListener('click', botaoCalculadora)
+
+function botaoJogoVelha(){
+    window.location.href = 'https://andreaasilva.github.io/jogo-da-velha/'
+}
+
+var segundo_botao = document.getElementById('projetos__botao__segundo__projeto')
+segundo_botao.addEventListener('click', botaoJogoVelha)
+
+function botaoPortfolio(){
+    window.location.href= 'https://projeto-portifolio-html-css.vercel.app/index.html'
+}
+
+var terceiro_botao = document.getElementById('projetos__botao__terceiro__projeto')
+terceiro_botao.addEventListener('click', botaoPortfolio)
+
+
+
+
