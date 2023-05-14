2419ee9624bab64f18893fa265ef2d3f40ffa5c5
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-05-14 17:31:21 -0300

    Incluído novo link
    Incluído página projeto pokemon no quinto botão

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
+const primeiro_botao = document.getElementById('projetos__botao__primeiro__projeto')
+primeiro_botao.addEventListener('click', botaoCalculadora)
+
 function botaoCalculadora(){
     window.location.href = 'https://andreaasilva.github.io/calculadora/'
 }
 
-var primeiro_botao = document.getElementById('projetos__botao__primeiro__projeto')
-primeiro_botao.addEventListener('click', botaoCalculadora)
+
+const segundo_botao = document.getElementById('projetos__botao__segundo__projeto')
+segundo_botao.addEventListener('click', botaoJogoVelha)
 
 function botaoJogoVelha(){
     window.location.href = 'https://andreaasilva.github.io/jogo-da-velha/'
 }
 
-var segundo_botao = document.getElementById('projetos__botao__segundo__projeto')
-segundo_botao.addEventListener('click', botaoJogoVelha)
+
+const terceiro_botao = document.getElementById('projetos__botao__terceiro__projeto')
+terceiro_botao.addEventListener('click', botaoPortfolio)
 
 function botaoPortfolio(){
     window.location.href= 'https://projeto-portifolio-html-css.vercel.app/index.html'
 }
 
-var terceiro_botao = document.getElementById('projetos__botao__terceiro__projeto')
-terceiro_botao.addEventListener('click', botaoPortfolio)
+
+const quarto_botao = document.getElementById('projetos__botao__quarto__projeto')
+quarto_botao.addEventListener('click', jogoForca)
 
 function jogoForca(){
     window.location.href='https://andreaasilva.github.io/jogo-da-forca/'
 }
 
-var quarto_botao = document.getElementById('projetos__botao__quarto__projeto')
-quarto_botao.addEventListener('click', jogoForca)
 
+const quinto_botao = document.getElementById('projetos__botao__quinto__projeto')
+quinto_botao.addEventListener('click', paginaPokemon)
+
+function paginaPokemon(){
+    window.location.href='https://andreaasilva.github.io/projeto-pokemon/'
+}
 
 
